07f85230f23e5834fe35292c2a461895cfaa2299
Yuma <yuma.ollivier@hotmail.com>
2021-05-24 09:28:19 +0200

    Sending Requests & Responses and Working with Postman

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const feedRoutes = require('./controllers/feed');
+const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Header', 'content-Type, Authorization');
+  next();
+});
 
 app.use('/feed', feedRoutes);
 
